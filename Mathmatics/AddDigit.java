828082ff062661de144a844f149d25c80ba7b01a
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-07-02 11:47:59 +0530

    Add Mathmatics folder with AddDigit solution

---
+++ Mathmatics/AddDigit.java
@@ -0,0 +1,39 @@
+package Mathmatics;
+
+//Optimized 258
+
+public class AddDigit {
+
+    public static int AddDigit(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n % 9 == 0) {
+            return 9;
+        }
+        return n % 9;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(AddDigit(1234));
+    }
+}
+
+// //Brute-force approach
+// public class AddDigit {
+// public static int addDigit(int num) {
+// int sum = 0;
+// while (num != 0) {
+// int lastdigit = num % 10;
+// sum += lastdigit;
+// num /= 10;
+// }
+// return sum;
+// }
+// public static int AddDig(int n) {
+// while (n > 9) {
+// n = addDigit(n);
+// }
+// return n;
+
+// }
