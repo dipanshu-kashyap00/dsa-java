0c70a1eb601355dddc9d036fab7ca66ae4642727
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-07-10 12:08:44 +0530

    Update Perfect Number solution

--- Mathmatics/PerfectNumber.java
+++ Mathmatics/PerfectNumber.java
@@ -1,7 +1,8 @@
 package Mathmatics;
 
-//507 Perfect Number Leetcode
-// A perfect number is a positive integer that is equal to the sum of its positive divisors,excluding the number itself.A divisor of an integer x is an integer that can divide x evenly.
+// A perfect number is a positive integer that is equal
+// to the sum of its positive divisors,excluding the
+//  number itself.A divisor of an integer x is an integer that can divide x evenly.
 
 // Given an integer n,return true if n is a perfect number,otherwise return false.
 
