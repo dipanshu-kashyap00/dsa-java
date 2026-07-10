2363e07dbb05e50446dfd8551f235ed3564b3581
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-07-10 12:08:45 +0530

    Add Move Zeroes

---
+++ Arrays/MovesZero.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class MovesZero {
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] != 0) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+            }
+        }
+
+    }
+
+}
