80f5971db6cde0c562257c099b5faf3b2ee223e3
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-07-15 10:31:24 +0530

    Add HashMap solutions

---
+++ HashMap/TwoSum.java
@@ -0,0 +1,24 @@
+package HashMap;
+
+import java.util.*;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (map.containsKey(comp)) {
+                return new int[] { map.get(comp), i };
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
