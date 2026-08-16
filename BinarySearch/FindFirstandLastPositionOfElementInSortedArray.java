cc6db728806477241abb0ce2b0b84cbaccdb0019
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-08-16 13:43:31 +0530

    Add BinarySearch problems: BinarySearch704, FindFirstandLastPosition, KokoEatingBananas, MergeSortedArray, SortColors

---
+++ BinarySearch/FindFirstandLastPositionOfElementInSortedArray.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class FindFirstandLastPositionOfElementInSortedArray {
+
+    public int[] searchRange(int[] nums, int target) {
+        int first = findBound(nums, target, true);
+        int last = findBound(nums, target, false);
+        return new int[] { first, last };
+    }
+
+    public int findBound(int[] nums, int target, boolean findFirst) {
+        int start = 0;
+        int end = nums.length - 1;
+        int result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                result = mid;
+                if (findFirst) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ BinarySearch/SortColors.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class SortColors {
+
+    public void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int one = 0;
+        while (one <= end) {
+            if (nums[one] == 2) {
+                swap(nums, one, end);
+                end--;
+            } else if (nums[one] == 1) {
+                one++;
+            } else if (nums[one] == 0) {
+                swap(nums, one, start);
+                start++;
+                one++;
+            }
+        }
+
+    }
+
+}
