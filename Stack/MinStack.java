15dcfbaceb147c4fc7fad0e3b49a32c78be586b9
dipanshu-kashyap00 <Dipanshukashyap250@gmail.com>
2026-09-01 10:46:58 +0530

    Fix/optimize MinStack implementation

--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -1,5 +1,7 @@
 package Stack;
+
 import java.util.*;
+
 class MinStack {
     Stack<Integer> stack;
     Stack<Integer> minStack;
@@ -7,35 +9,37 @@ class MinStack {
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
-         
+
     }
-    
+
     public void push(int val) {
 
         stack.push(val);
-        if(minStack.isEmpty() || val <=minStack.peek()){
+        if (minStack.isEmpty() || val <= minStack.peek()) {
             minStack.push(val);
-        }        
+        }
     }
 
     public void pop() {
-    if(stack.isEmpty()) return; 
-    
-    int removed = stack.pop();
-    if(removed == minStack.peek()){
-        minStack.pop();
-    }
-  }
+        if (stack.isEmpty())
+            return;
 
-    
-    public int top() {
-return stack.peek();
-        
+        int removed = stack.pop();
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
     }
-    
+
+    public int top() {
+        return stack.peek();
+
+    }
+
     public int getMin() {
 
         return minStack.peek();
         
     }
+    
 }
+
